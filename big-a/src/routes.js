1eaf0b83d3850a9f3eb52138a2cef55a7ae058bf
Josh <hlee43@msudenver.edu>
2024-03-29 00:58:46 -0600

    adjust default home pathing to dashboard

--- big-a/src/routes.js
+++ big-a/src/routes.js
@@ -52,7 +52,7 @@ const Toasts = React.lazy(() => import('./views/notifications/toasts/Toasts'))
 const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
 
 const routes = [
-  { path: '/', exact: true, name: 'Home' },
+  { path: '/', exact: true, name: 'Dashboard', element: Dashboard },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/playbook', name: 'Playbook', element: GenerateAndViewYaml },
   { path: '/theme', name: 'Theme', element: Colors, exact: true },
